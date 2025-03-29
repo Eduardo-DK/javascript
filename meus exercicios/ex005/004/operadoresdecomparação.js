215f6ee58decd88d09d000ce8e394b763db97fca
Eduardo-DK <drudack45@gmail.com>
2025-03-28 21:20:35 -0300

    Update operadoresdecomparação.js

--- meus exercicios/ex005/004/operadoresdecomparação.js
+++ meus exercicios/ex005/004/operadoresdecomparação.js
@@ -1,6 +1,13 @@
 let n1 = 5
 let n2 = 2
 
-console.log(
-    
-)
+console.log(`
+    ${n1 > n2}
+    ${n1 < n2}
+    ${n1 >= n2}
+    ${n1 <= n2}
+    ${n1 == n2}
+    ${n1 != n2}
+    ${n1 === n2}
+    ${n1 !== n2}
+`);
