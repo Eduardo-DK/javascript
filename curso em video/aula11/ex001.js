5272d60da54199eddac464cc3726e59400fad52c
Eduardo-DK <drudack45@gmail.com>
2025-05-05 18:41:21 -0300

    Create ex001.js

---
+++ curso em video/aula11/ex001.js
@@ -0,0 +1,7 @@
+var vel = 81
+console.log(`A velocidade do seu carro é de ${vel} Km/h`)
+if (vel > 80) {
+    console.log(`Voce ultrapassou o limeite de velocidade, MULTADO`)
+}
+console.log(`Dirija usando sempre o cinto de segurança`)
+
