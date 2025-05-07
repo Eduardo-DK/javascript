ed63359d69a2a97e97da045651e81b66192b002a
Eduardo-DK <drudack45@gmail.com>
2025-05-07 17:57:08 -0300

    Update script.js

--- curso em video/exercicios/ex002/script.js
+++ curso em video/exercicios/ex002/script.js
@@ -9,6 +9,42 @@ function verificar() {
     else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'homem'
+            if (idade < 10) {
+                // criança
+                img.setAttribute('src', 'criançaH.jpg')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'jovemH.jpg')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'adultoH.jpg')
+            } else {
+                // idoso
+                img.setAttribute('src', 'H.jpg')
+            }
+        }
+        else if (fsex[1].checked) {
+            genero = 'homem'
+            if (idade < 10) {
+                // criança
+                img.setAttribute('src', 'criançaM.jpg')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'jovemM.jpg')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'adultoM.jpg')
+            } else {
+                // idoso
+                img.setAttribute('src', 'idosoM.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `detectado ${genero} idade ${idade}`
+        res.appendChild(img)
     }
     
 }
