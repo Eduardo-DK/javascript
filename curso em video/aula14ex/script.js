fac9d29c0bef18e2b4d3e38b5e53574a78f5394d
Eduardo-DK <drudack45@gmail.com>
2025-05-12 18:13:54 -0300

    ex 5 curso em video

--- curso em video/aula14ex/script.js
+++ curso em video/aula14ex/script.js
@@ -1,9 +1,30 @@
 function contar() {
-    var ini = window.document.getElementById('txti')
+    var ini = document.getElementById('txti');
     var fim = document.getElementById('txtf')
     var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
 
-    if (ini.value.lenght) {
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('erro')
+    } else {
+        res.innerHTML = 'contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('pasos invalido')
+        }
+        if (i < f) {
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} `
+            }
+        } else {
+            for(let c = i; c >= f; c -= p){
+               res.innerHTML += `${c}` 
+            }
+        }
         
     }
+
+    
 }
