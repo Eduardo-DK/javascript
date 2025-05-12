015f81490bac6385bb88d9c33773e5065e6f85dd
Eduardo-DK <drudack45@gmail.com>
2025-05-12 17:33:25 -0300

    excv

---
+++ curso em video/aula14ex/script.js
@@ -0,0 +1,9 @@
+function contar() {
+    var ini = window.document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+
+    if (ini.value.lenght) {
+        
+    }
+}
