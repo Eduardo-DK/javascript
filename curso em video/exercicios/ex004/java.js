982d1070533d3c0cfbcb09b0d947c06c59014859
Eduardo-DK <drudack45@gmail.com>
2025-05-17 09:52:33 -0300

    ex4 cursvideo

--- curso em video/exercicios/ex004/java.js
+++ curso em video/exercicios/ex004/java.js
@@ -1,7 +1,7 @@
 let num = document.getElementById('fnum')   
 let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
-let valores = ['1']
+let valores = []
 
 function isNumero(n) {
     if(Number(n) >= 1 && Number(n) <= 100){
@@ -13,13 +13,41 @@ function isNumero(n) {
 }
 
 function inlist(vetor, num) {
-    return !vetor.includes(num)
+    let n = (Number(num.value))
+    return !vetor.includes(n)
 }
 
 function adicionar() {
-    if (isNumero(num.value) && inlist(valores, num.value)) {
-        alert('ok')
+    
+    if (isNumero(num.value) && inlist(valores, num)) {
+        valores.push (Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         alert('valor invalido')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('adicionar valores')
+    } else {    
+        let totalN = valores.length
+        let maior = Math.max(...valores)
+        let soma = 0
+        let media = soma / totalN
+        for(let i = 0; i < valores.length; i++)
+            soma += valores[i]
+        
+        res.innerHTML = [`
+            o tamanho do vetor ${totalN} <br>   
+            o maior numero é ${maior} <br>
+            a soma entre os numeros é ${soma} <br>
+            a media entre os numeros é ${media} <br>
+        `]
     }
 }
